cff5db3d235d5eba34a64cf4fe90a3ff3061e70b
Lucas Bach <lborgesbach@gmail.com>
2024-03-20 16:27:50 -0300

    registration filter

---
+++ basecod_cadastro.py
@@ -0,0 +1,17 @@
+# file = open("lista.csv",'r')
+
+# cadastros = []
+# n = 0
+
+# while True:
+#     line =  file.readline() 
+#     if not line or n > 1000:
+#         break
+#     cadastros.append(line)
+#     n += 1
+
+
+# for i,cadastro in enumerate(cadastros):
+#     linha_cadastro = cadastro.split(",")
+#     print(cadastro)
+#     print(i)
--- cadastro.py
+++ cadastro.py
@@ -1,22 +1,3 @@
-# file = open("lista.csv",'r')
-
-# cadastros = []
-# n = 0
-
-# while True:
-#     line =  file.readline() 
-#     if not line or n > 1000:
-#         break
-#     cadastros.append(line)
-#     n += 1
-
-
-# for i,cadastro in enumerate(cadastros):
-#     linha_cadastro = cadastro.split(",")
-#     print(cadastro)
-#     print(i)
-
-
 with open("lista.csv", 'r') as file:
     linhas = file.readlines()
 
@@ -27,8 +8,8 @@ cadastros = []
 for linha in linhas[1:]:    
     campos = linha.strip().split(",")
 
-    state = campos[4].strip('()')
-    namestate = campos[5].strip('()')
+    state = campos[4].strip('()').replace("'", "")
+    namestate = campos[5].strip('()').replace("'", "").replace(" ", "")
     
     cadastro = {
         "name": campos[0],
@@ -37,11 +18,85 @@ for linha in linhas[1:]:
         "sexo": campos[3],
         "state": state,
         "namestate": namestate,
-        "city": campos[6]
-    }
-    
+        "city": campos[6],
+
+    }   
+
+
     cadastros.append(cadastro)
 
 
-for i, cadastro in enumerate(cadastros[:1]):
+for i, cadastro in enumerate(cadastros[:50]):
     print(f"Cadastro {i+1}: {cadastro}")
+
+
+
+# estado_desejado = "SC"
+# genero_masculino = "M"
+# contatos_encontrados = 0
+# usuarios_encontrados = 0
+# contatos_estado = []
+# usuarios_masculinos = []
+
+# for cadastro in cadastros:
+#     if cadastro["state"] == estado_desejado:
+#         contatos_encontrados += 1
+#         contatos_estado.append(cadastro)
+#         if contatos_encontrados == 10:
+#             break
+
+#     if cadastro["sexo"] == genero_masculino:
+#         usuarios_encontrados += 1
+#         usuarios_masculinos.append(cadastro)
+#         if usuarios_encontrados == 10:
+#             break
+
+# print("Contatos do estado desejado:")
+# for i, contato in enumerate(contatos_estado):
+#     print(f"Contato {i+1}: {contato}")
+
+# print("\nUsuários masculinos:")
+# for i, contato in enumerate(usuarios_masculinos):
+#     print(f"Contato {i+1}: {contato}")
+
+
+
+estado_desejado = "SC"
+contatos_encontrados = 0
+contatos_estado = []
+
+
+
+for cadastro in cadastros:
+    
+    if cadastro["state"] == estado_desejado:
+        
+        contatos_encontrados += 1
+        
+        contatos_estado.append(cadastro)
+        
+        
+        if contatos_encontrados == 10:
+            break
+
+
+for i, contato in enumerate(contatos_estado):
+    print(f"Contato {i+1}: {contato}")
+
+genero_masculino = "M"
+usuarios_encontrados = 0
+usuarios_masculinos = []    
+
+for cadastro in cadastros:
+
+    if cadastro["sexo"] == genero_masculino:
+
+        usuarios_encontrados += 1
+
+        usuarios_masculinos.append(cadastro)
+
+        if usuarios_encontrados == 10:    
+            break
+
+for i, contato in enumerate(usuarios_masculinos):
+    print(f"Contato {i+1}: {contato}")
