85a26ffd7a26cab2d38dd907835734bfca6a3890
Lucas Bach <lborgesbach@gmail.com>
2024-04-01 15:06:06 -0300

    func/inserir_cadastro

--- cadastro.py
+++ cadastro.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+import sqlite3
 
 class Cadastro(BaseModel):
     name: str
@@ -38,29 +39,49 @@ def pesquisar_por_sexo(cadastros, sexo, limite = 10):
     
     return resultados
 
+#colocar como função
+def ler_csv():
+    with open("lista.csv", 'r') as file:
+        linhas = file.readlines()
 
-with open("lista.csv", 'r') as file:
-    linhas = file.readlines()
+    cadastros = []
 
-cadastros = []
+    for linha in linhas[1:]:
+        campos = linha.strip().split(",")
+        
+        state = campos[4].strip('()').replace("'", "")
+        namestate = campos[5].strip('()').replace("'", "").replace(" ", "")
+        
+        cadastro = {
+            "name": campos[0],
+            "phone": campos[1],
+            "cpf": campos[2],
+            "sexo": campos[3],
+            "state": state,
+            "namestate": namestate,
+            "city": campos[6],
+        }
+        
+        cadastros.append(cadastro)
+    return cadastros
 
-for linha in linhas[1:]:
-    campos = linha.strip().split(",")
-    
-    state = campos[4].strip('()').replace("'", "")
-    namestate = campos[5].strip('()').replace("'", "").replace(" ", "")
-    
-    cadastro = {
-        "name": campos[0],
-        "phone": campos[1],
-        "cpf": campos[2],
-        "sexo": campos[3],
-        "state": state,
-        "namestate": namestate,
-        "city": campos[6],
-    }
-    
-    cadastros.append(cadastro)
+
+def inserir_cadastro(nome):
+    conn = sqlite3.connect("testnovo.db")
+    cursor = conn.cursor()
+    query = f"INSERT INTO pessoa (nome) VALUES (?);"
+    cursor.execute(query,(nome,))
+    conn.commit()
+    conn.close()
+
+
+
+
+
+
+
+
+
 
 
 
